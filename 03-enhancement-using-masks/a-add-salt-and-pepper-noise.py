c37750360ab51cd4a9f511ae93bb66cc181f693a
Davi Uezono <davi.uezono@gmail.com>
2015-04-04 21:41:09 -0300

    done! uhuuullgit add * report missing only.

--- 03-enhancement-using-masks/a-add-salt-and-pepper-noise.py
+++ 03-enhancement-using-masks/a-add-salt-and-pepper-noise.py
@@ -12,37 +12,41 @@ while name not in ["coins", "columns", "dragon"]:
 
 image = open(name + ".pgm", "r")
 if image.readline() == "P2\n":
-  # ignore comment line from PGM format
+  # ignore comment line from PGM format.
   image.readline()
 
   size_string = image.readline()
   size = size_string.split()
-  
-  # number of pixels in image
-  n_pixels = int (size[0]) * int (size[1])
-  # number of pixels in image to apply salt and pepper
-  sp_pixels = int (n_pixels * percentage / 100)
+
+  # number of pixels in image.
+  n_pixels = int(size[0]) * int(size[1])
+  # number of pixels in image to apply salt and pepper.
+  sp_pixels = int(n_pixels * percentage / 100)
   
   depth_string = image.readline()
   depth = depth_string.split()
-  depth = int (depth[0])
-  
-  salt_and_pepper.write("# created by Davi K. Uezono - RA 097464\n")
+  depth = int(depth[0])
+
+  # creating the new image with salt and pepper effect.
   salt_and_pepper = open(name + "_adding_salt_pepper.pgm", "w")
   salt_and_pepper.write("P2\n")
+  salt_and_pepper.write("# created by Davi K. Uezono - RA 097464\n")
   salt_and_pepper.write(size_string)
   salt_and_pepper.write(depth_string)
 
-
-
-  #counter = 0
   lines = image.readlines()
-  for i in range(0, len(lines)):
+  for i in range(len(lines)):
     values_in_this_line = lines[i].split()
     for pixel in values_in_this_line:
-      #counter = counter + 1
-      max_prob = int (100 / percentage) - 1
+      # when I have a colection of (max_prob + 1) elements, if I choose one of
+      # them arbitrary, I will have the same percentage of choosing one of them
+      # as defined in the very beginning of this source code.
+      max_prob = int(100 / percentage) - 1
+      # the random int between zero and max_prob converted to a boolean value
+      # and negated will return a TRUE value in the same percentage as defined
+      # in the very beginning of this source code.
       if(not bool(randint(0,max_prob)) and bool(sp_pixels > 0)):
+        # arbitrary BLACK or WHITE dots in the image.
         pixel_out = str(depth * randint(0,1))
         salt_and_pepper.write(pixel_out)
         sp_pixels = sp_pixels - 1
--- 03-enhancement-using-masks/b-remove-salt-and-pepper-noise.py
+++ 03-enhancement-using-masks/b-remove-salt-and-pepper-noise.py
@@ -10,57 +10,55 @@ name = raw_input("Please select one of these images: coins, columns or dragon.\n
 while name not in ["coins", "columns", "dragon"]:
   name = raw_input("ERROR! Please select one of these images: coins, columns or dragon.\n")
 
-image = open(name + "_adding_salt_pepper.pgm", "r")
-if image.readline() == "P2\n":
+salt_and_pepper = open(name + "_adding_salt_pepper.pgm", "r")
+if salt_and_pepper.readline() == "P2\n":
+  # ignore comment line from PGM format.
+  salt_and_pepper.readline()
 
-  # ignore comment line from PGM format
-  image.readline()
-
-  size_string = image.readline()
+  size_string = salt_and_pepper.readline()
   size = size_string.split()
   n_columns = int(size[0])
   n_lines = int(size[1])
 
-  depth_string = image.readline()
+  depth_string = salt_and_pepper.readline()
   depth = depth_string.split()
-  depth = int (depth[0])
+  depth = int(depth[0])
 
-  # reading the image into a pixel list
+  # reading the input image into a pixel list.
   pixel_list = []
-  lines = image.readlines()
+  lines = salt_and_pepper.readlines()
   for i in range(len(lines)):
     values_in_this_line = lines[i].split()
     for pixel in values_in_this_line:
       pixel_list.append(int(pixel))
-  image.close()
+  salt_and_pepper.close()
 
+  # output pixel list initialized as the same of input one.
   pixel_list_out = pixel_list
   mw = int(mask/2)
 
+  # rastering the input image including borders.
   for i in range(n_lines):
     for j in range(n_columns):
-
       mask_of_pixels = []
-      count = 0
-      
       for x in range (i - mw, i + mw + 1):
         for y in range (j - mw, j + mw + 1):
+          # ignoring negative bounds.
           if (x >= 0 and x < n_lines and y >= 0 and y < n_columns):
-            count = count + 1
             mask_of_pixels.append(pixel_list[x*n_columns + y])
-
-      print count
+      # median value is the central value of an ordered vector.
       mask_of_pixels.sort()
       median = int(len(mask_of_pixels)/2)
       pixel_list_out[i*n_columns + j] = mask_of_pixels[median]
 
-  # creating the final image
+  # initializing the final (clean) image.
   clean_image = open(name + "_removing_salt_pepper.pgm", "w")
   clean_image.write("P2\n")
   clean_image.write("# created by Davi K. Uezono - RA 097464\n")
   clean_image.write(size_string)
   clean_image.write(depth_string)
 
+  # printing final pixel values in the final (clean) image.
   for i in range(n_lines):
     for j in range(n_columns):
       clean_image.write(str(pixel_list_out[i*n_columns + j]) + " ")
